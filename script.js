89c9d2d1aa9f2b6b0c2b8ffed4ba4bb4dac08e46
daniel-kav <dankavatha01@gmail.com>
2023-08-19 18:14:48 +0300

    gameboard javascript

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
+
+let gameboard = (() => {
+    let gameboard = ["", "", "", "", "", "", "", "",""];
+
+    const render = () => {
+        let boardHTML = "";
+        gameboard.forEach((square, index) => {
+            boardHTML += `<div class="square" id="${index}">${square}</div>`;
+        });
+    }
+})
+
 const startButton = document.querySelector("#startButton");
 starrtButton.addEventListener("click", () => {
-    Gamepad.start();
+    //Game.start();
 });
